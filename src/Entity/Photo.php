17f20eb5c42e26416a6a85609789ead02197dc31
Kaja <kaja.michalik@student.uj.edu.pl>
2026-06-19 14:09:20 +0200

    Initial working version of gallery project

--- src/Entity/Photo.php
+++ src/Entity/Photo.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PhotoRepository::class)]
 class Photo
@@ -17,7 +18,13 @@ class Photo
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    
+
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $title = null;
+
+
 
     #[ORM\Column(length: 255)]
     private ?string $filename = null;
@@ -32,10 +39,12 @@ class Photo
     private Collection $tags;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+#[Assert\Length(max: 2000)]
     private ?string $description = null;
 
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
     private ?\DateTimeImmutable $createdAt = null;
+    
 
     public function __construct()
     {
