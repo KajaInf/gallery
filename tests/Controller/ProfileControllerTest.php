087b241d2b59a1acfd9222f5477381792dfdd9ff
Kaja <kaja.michalik@student.uj.edu.pl>
2026-06-21 11:58:46 +0200

    Add profile controller test

---
+++ tests/Controller/ProfileControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProfileControllerTest extends WebTestCase
+{
+    public function testProfileRedirectsAnonymousUser(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/profile');
+
+        $this->assertResponseRedirects();
+    }
+}
