504e438c2f9d326bc34e3a0b21978bfbb385494d
Kaja <kaja.michalik@student.uj.edu.pl>
2026-06-21 13:06:10 +0200

    security controller test

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testLoginPageIsSuccessful(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form');
+    }
+}
