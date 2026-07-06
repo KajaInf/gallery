f149af60eda43d3d8b42a7ccd59715542c92332a
Kaja <kaja.michalik@student.uj.edu.pl>
2026-07-06 03:31:55 +0200

    Separate profile data and password forms

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Form\ProfilePasswordType;
 
 /**
  * Class ProfileController.
@@ -41,9 +42,6 @@ final class ProfileController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $plainPassword = $form->get('plainPassword')->getData();
-
-            $userService->updatePassword($user, $plainPassword);
             $userService->save($user);
 
             $this->addFlash('success', 'Dane profilu zostały zaktualizowane.');
@@ -53,6 +51,41 @@ final class ProfileController extends AbstractController
 
         return $this->render('profile/index.html.twig', [
             'profile_form' => $form,
+        ]);
+    }
+
+/**
+ * Password action.
+ *
+ * @param Request              $request     HTTP request
+ * @param UserServiceInterface $userService User service
+ *
+ * @return Response HTTP response
+ */
+    #[Route('/profile/password', name: 'app_profile_password', methods: ['GET', 'POST'])]
+    public function password(Request $request, UserServiceInterface $userService): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $form = $this->createForm(ProfilePasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            $userService->updatePassword($user, $plainPassword);
+            $userService->save($user);
+
+            $this->addFlash('success', 'Hasło zostało zmienione.');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/password.html.twig', [
+            'password_form' => $form,
         ]);
     }
 }
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -8,8 +8,6 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -27,18 +25,8 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'mapped' => false,
-                'required' => false,
-                'first_options' => [
-                    'label' => 'Nowe hasło',
-                ],
-                'second_options' => [
-                    'label' => 'Powtórz nowe hasło',
-                ],
-            ]);
+        ->add('email')
+        ;
     }
 
     /**
